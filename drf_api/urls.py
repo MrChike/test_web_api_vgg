1ea0d70e31798862b8df8a93947115290c6c4bb9
MrChike <chikeegonu@gmail.com>
2020-02-07 09:38:33 +0100

    classBased djangorestframework View setup

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # from core.views import test_view -- A new method of importing views
 from core import views
 
+
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', views.test_view, name='test'),
+    path('', views.TestView.as_view(), name='test'),
 ]
