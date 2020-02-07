8a3c8622cfea4d869a201fb0bd814117bf2159c1
MrChike <chikeegonu@gmail.com>
2020-02-07 08:55:06 +0100

    ran migrate command

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import test_view
+# from core.views import test_view -- A new method of importing views
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
